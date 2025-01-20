0a29b57a28e7861a96d85fd9d62a4bc475da523b
Your Name <aminemaroui145@gmail.com>
2025-01-20 02:25:03 +0100

    rename

--- src/test/java/org/sid/clubservice/ClubServiceTest.java
+++ src/test/java/org/sid/clubservice/ClubServiceTest.java
@@ -17,7 +17,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-public class AnotherClubServiceTest {
+public class ClubServiceTest {
 
         @Mock
         private ClubRepository clubRepository;
